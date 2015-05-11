deb938c44344e68673b13a7bd86644623e26290f
jcamilogironm <juancamilo1019@hotmail.com>
2015-05-11 17:03:45 -0500

    Modificacion clase splash activity Creacion de tabla activity Adicion de la imagen del splash Modificacion activity splash creacion de el activity tabla Modificacion strings Modificacion Manifest

--- app/src/main/java/com/example/juancamilogiron/ligaaguila/SplashActivity.java
+++ app/src/main/java/com/example/juancamilogiron/ligaaguila/SplashActivity.java
@@ -1,17 +1,87 @@
 package com.example.juancamilogiron.ligaaguila;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class SplashActivity extends ActionBarActivity {
 
+    boolean spActive;
+    boolean spPaused;
+    long spTime=3000;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        spPaused=false;
+        spActive=true;
+
+        Thread splashTimer=new Thread(){
+
+            public void run(){
+                try{
+                    long ms=0;
+                    while(spActive&&ms<spTime){
+                        sleep(100);
+
+                        if (!spPaused)
+                            ms+=100;
+
+                    }
+
+                    startActivity(new Intent("com.example.juancamilogiron.CLEARSPLASH"));
+
+                    finish();
+
+                }catch(Exception e){
+
+                    System.out.println(e.toString());
+
+                }
+
+
+
+            }
+
+
+        };
+
+        splashTimer.start();
         setContentView(R.layout.activity_splash);
+        return;
+    }
+
+    protected void onStop(){
+
+        super.onStop();
+
+    }
+
+    protected void onPause(){
+        super.onPause();
+        spPaused=true;
+    }
+
+    protected void onResume(){
+
+        super.onResume();
+        spPaused=false;
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+
+    }
+
+    public boolean onKeyDown(int keyCode,KeyEvent event){
+
+        super.onKeyDown(keyCode,event);
+        spActive=false;
+        return true;
     }
 
     @Override
---
+++ app/src/main/java/com/example/juancamilogiron/ligaaguila/TablaActivity.java
@@ -0,0 +1,11 @@
+package com.example.juancamilogiron.ligaaguila;
+
+import android.app.Activity;
+
+/**
+ * Created by Juan Camilo Giron on 11/05/2015.
+ */
+public class TablaActivity extends Activity {
+
+
+}
