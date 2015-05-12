d3159f50b37c20c1eb70eadc44a7952c8ca7a52f
jcamilogironm <juancamilo1019@hotmail.com>
2015-05-12 11:17:02 -0500

    Adicion del boton de actualizacion del action bar, modificacion spash y tabla activity, manifest

--- app/src/main/java/com/example/juancamilogiron/ligaaguila/SplashActivity.java
+++ app/src/main/java/com/example/juancamilogiron/ligaaguila/SplashActivity.java
@@ -4,8 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 
 public class SplashActivity extends ActionBarActivity {
@@ -83,25 +82,5 @@ public class SplashActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
