8a173330e1c9c21278c153505a0fc4c50b18b524
jcamilogironm <juancamilo1019@hotmail.com>
2015-05-11 21:59:44 -0500

    Modificacion tabla activity y activity tabla

--- app/src/main/java/com/example/juancamilogiron/ligaaguila/TablaActivity.java
+++ app/src/main/java/com/example/juancamilogiron/ligaaguila/TablaActivity.java
@@ -1,11 +1,14 @@
 package com.example.juancamilogiron.ligaaguila;
 
 import android.app.Activity;
+import android.os.Bundle;
 
-/**
- * Created by Juan Camilo Giron on 11/05/2015.
- */
+
 public class TablaActivity extends Activity {
 
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabla);
+    }
 }
